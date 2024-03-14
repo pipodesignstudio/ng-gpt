d43aa7d8fc8216cc355e9bb7f516acb8d7efb307
Pipo_Studio <67048178+pipodesignstudio@users.noreply.github.com>
2024-03-14 15:51:30 +0100

    ok

--- src/app/presentation/pages/imageGeneration/imageGeneration.component.ts
+++ src/app/presentation/pages/imageGeneration/imageGeneration.component.ts
@@ -1,11 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
+import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
+import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
+import { Message } from '@interfaces/message.interface';
+import { OpeanAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-image-generation',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './imageGeneration.component.html',
   styles: `
@@ -15,4 +27,38 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ImageGenerationComponent { }
+export default class ImageGenerationComponent {
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpeanAiService);
+
+
+
+  handleMessage(prompt: string) {
+
+    this.isLoading.set(true);
+    this.messages.update( prev => [...prev, { isGpt:false, text: prompt }]  );
+
+    // this.openAiService.imageGeneration(prompt)
+    //   .subscribe( resp => {
+    //     this.isLoading.set(false);
+    //     if ( !resp ) return;
+
+    //     this.messages.update(prev => [
+    //       ...prev,
+    //       {
+    //         isGpt: true,
+    //         text: resp.alt,
+    //         imageInfo: resp,
+    //       }
+    //     ]);
+
+
+    //   })
+
+
+
+
+
+  }
+ }
